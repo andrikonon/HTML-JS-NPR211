d8153f7222cbfbd3106d80b646bbdd62542dd8df
andrikonon <andri.konon@gmail.com>
2024-06-16 12:22:06 +0200

    Forms now validate profile picture file

---
+++ 1.SimpleHTML/js/file_input.js
@@ -0,0 +1,20 @@
+(() => {
+
+    const input = document.getElementById('user-pfp')
+    input.addEventListener("change", e => {
+        const img = document.getElementById('pfp-preview')
+        const file = e.target.files[0]
+        if (file) {
+            if (file.type.startsWith('image/')) {
+                img.src = window.URL.createObjectURL(file)
+            } else {
+                alert('Тип файлу не картинка')
+                img.src = 'images/user.jpg'
+                e.target.value = ''
+            }
+        }
+        else {
+            alert('Оберіть файл!')
+        }
+    })
+})()
