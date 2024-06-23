6d11753d0c62be79d5424dc4b660612d0fe54f61
andrikonon <andri.konon@gmail.com>
2024-06-23 12:19:28 +0200

    Refactored validation code

--- 1.SimpleHTML/js/validation.js
+++ 1.SimpleHTML/js/validation.js
@@ -1,16 +1,20 @@
-(() => {
+const validate = f => {
     'use strict'
 
     const forms = document.querySelectorAll('.needs-validation')
 
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
+            const valid = form.checkValidity()
+            event.preventDefault()
+            if (!valid) {
                 event.stopPropagation()
+            } else {
+                console.log("Form submit valid", form.id)
+                f()
             }
 
             form.classList.add('was-validated')
         }, false)
     })
-})()
+}
